d8da7b78244a9f1369169063bc33a1f8cadf66e1
ReginaFG <a01352605@tec.mx>
2024-11-08 19:15:20 -0600

    Game.cpp

---
+++ Game.cpp
@@ -0,0 +1,8 @@
+Game::Game(const std::string &gName, const std::string &pName) 
+    : gameName(gName), player(pName) {}
+
+void Game::addQuestion(const Question &q) {}
+
+void Game::startGame() {}
+
+void Game::displayLeaderboard() {}
