7634c4409a1541764340a9cc3ac493795f6cba13
ReginaFG <a01352605@tec.mx>
2024-11-08 19:16:45 -0600

    Timer.cpp

---
+++ Timer.cpp
@@ -0,0 +1,9 @@
+#include "Timer.h"
+
+Timer::Timer(int duration) : duration(duration), remainingTime(duration) {}
+
+void Timer::start() {}
+
+void Timer::stop() {}
+
+int Timer::getRemainingTime() { return 0; }
