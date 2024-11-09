671f30a692674dd219660b72db396a0fd2aff204
ReginaFG <a01352605@tec.mx>
2024-11-08 19:14:01 -0600

    Player.cpp

---
+++ Player.cpp
@@ -0,0 +1,9 @@
+#include "Player.h"
+
+Player::Player(const std::string &n) : name(n), score(0) {}
+
+std::string Player::getName() { return ""; }
+
+void Player::updateScore(int points) {}
+
+int Player::getScore() const { return 0; }
