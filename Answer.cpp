cde624e4f7d9848ca88711b0799727ced64fb279
ReginaFG <a01352605@tec.mx>
2024-11-15 22:53:18 -0600

    Delete Answer.cpp

--- Answer.cpp
+++
@@ -1,7 +0,0 @@
-#include "Answer.h"
-
-Answer::Answer(std::string t, bool correct) : text(t), isCorrect(correct) {}
-
-std::string Answer::getText() { return ""; }
-
-bool Answer::isCorrectAnswer() { return false; }
